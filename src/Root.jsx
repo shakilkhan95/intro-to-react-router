32a663c8d6a0f595a22a281496807a9fdb142ff0
shakilkhan95 <th.shakilkhan@gmail.com>
2026-04-06 04:02:53 +0400

    practicing useNavigation();

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import Footer from './Footer';
 import Header from './Header';
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div>
       <h1>Welcome to React Router</h1>
       <Header></Header>
+      {isNavigating && <span style={{fontSize: '72px', color:'red'}}>Loading...</span>}
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
